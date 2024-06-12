72431913b6478889ba73e0eb6b6192f539653f47
Tin Vaiti <tin.vaiti5@gmail.com>
2024-06-12 14:05:11 +0200

    bugtest7 ' u "

--- router.php
+++ router.php
@@ -1,30 +1,30 @@
 <?php
 
-$requestUri = $_SERVER['REQUEST_URI'];
+$requestUri = $_SERVER["REQUEST_URI"];
 
 
 // Remove query string from the request URI
-$requestUri = explode('?', $requestUri, 2)[0];
+$requestUri = explode("?", $requestUri, 2)[0];
 
 // Remove leading slash and the prefix
-//$requestUri = preg_replace('~^/Digitalni-dom/~', '', $requestUri);
+//$requestUri = preg_replace("~^/Digitalni-dom/~", "", $requestUri);
 
-include('app/pocetna.php');
+include("pocetna.php");
 
 // If the request is for the root directory, load the default index.php
-if ($requestUri == '') {
-    $requestUri = 'index.php';
+if ($requestUri == "") {
+    $requestUri = "index.php";
 } else {
     // Replace slashes with underscores and append .php extension
-    $requestUri = str_replace('/', '_', $requestUri) . '.php';
+    $requestUri = str_replace("/", "_", $requestUri) . ".php";
 }
 
 /* // Check if the file exists
 if (file_exists($requestUri)) {
     include($requestUri);
 } else {
-    // If the file doesn't exist, return a 404 response
+    // If the file doesn"t exist, return a 404 response
     http_response_code(404);
-    include('pocetna.php');
+    include("pocetna.php");
 } */
 ?>
