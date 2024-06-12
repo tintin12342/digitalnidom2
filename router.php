3d65985a76625e448dbd7645595266711f21b09c
Tin Vaiti <tin.vaiti5@gmail.com>
2024-06-12 14:41:58 +0200

    gpt nes

--- router.php
+++ router.php
@@ -8,12 +8,15 @@ $requestUri = explode('?', $requestUri, 2)[0];
 // Remove trailing slashes
 $requestUri = rtrim($requestUri, '/');
 
-// Remove .php extension if present
+// Check if the request is for a PHP file
 if (substr($requestUri, -4) === '.php') {
-    $requestUri = substr($requestUri, 0, -4);
+    // Remove the .php extension
+    $cleanUrl = substr($requestUri, 0, -4);
+} else {
+    $cleanUrl = $requestUri;
 }
 
 // Redirect to the clean URL
-header("Location: https://digitalnidom2.up.railway.app/pocetna.php", false, 301);
+header("Location: https://digitalnidom2.up.railway.app$cleanUrl", false, 301);
 exit();
 ?>
