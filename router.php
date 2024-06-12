17a510c003928f30015a2d330e50abbfcff597c0
Tin Vaiti <tin.vaiti5@gmail.com>
2024-06-12 14:09:58 +0200

    bugtest8

--- router.php
+++ router.php
@@ -2,29 +2,27 @@
 
 $requestUri = $_SERVER["REQUEST_URI"];
 
-
-// Remove query string from the request URI
-$requestUri = explode("?", $requestUri, 2)[0];
-
-// Remove leading slash and the prefix
-//$requestUri = preg_replace("~^/Digitalni-dom/~", "", $requestUri);
-
-include("pocetna.php");
-
-// If the request is for the root directory, load the default index.php
-if ($requestUri == "") {
-    $requestUri = "index.php";
-} else {
-    // Replace slashes with underscores and append .php extension
-    $requestUri = str_replace("/", "_", $requestUri) . ".php";
-}
-
-/* // Check if the file exists
+// Check if the file exists
 if (file_exists($requestUri)) {
     include($requestUri);
 } else {
     // If the file doesn"t exist, return a 404 response
     http_response_code(404);
     include("pocetna.php");
+}
+
+
+/* // Remove query string from the request URI
+$requestUri = explode("?", $requestUri, 2)[0];
+
+// Remove leading slash and the prefix
+//$requestUri = preg_replace("~^/Digitalni-dom/~", "", $requestUri);
+
+// If the request is for the root directory, load the default index.php
+if ($requestUri == "") {
+    $requestUri = "pocetna.php";
+} else {
+    // Replace slashes with underscores and append .php extension
+    $requestUri = str_replace("/", "_", $requestUri) . ".php";
 } */
 ?>
